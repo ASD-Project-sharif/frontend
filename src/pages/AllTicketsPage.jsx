39954a86643f6440dd9fe7e3037f14dcbf25f112
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-01-01 18:31:09 +0330

    implement filter

--- src/pages/AllTicketsPage.jsx
+++ src/pages/AllTicketsPage.jsx
@@ -5,6 +5,10 @@ import { Table, theme } from "antd";
 import { Header } from "antd/es/layout/layout";
 import { limit } from 'stringz';
 import dayjs from 'dayjs';
+import Filter from "../components/filter";
+import { useState } from "react";
+import config from "../config/config";
+import axios from "axios";
 
 const persianNumbers = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰'];
 const englishNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
@@ -37,10 +41,31 @@ const AllTicketsPage = () => {
         token: { colorBgContainer },
     } = theme.useToken();
 
+    const pageSize = 20;
+
+    const [filter, setFilter] = useState({});
+    // const [sort, setSort] = useState();
+    // const [sortBy, setSortBy] = useState()
+
+    // useEffect(() => {
+    //     (async () => {
+    //         try {
+    //             await axios.post(
+    //                 `${config.baseUrl}/api/v1/ticket`,
+    //                 {
+
+    //                 }
+    //               );
+    //         } catch (error) {}
+    //       })();
+
+    // }, [filter]);
+
     const tickets = [{
         "user": "ali",
         "message": "الگویی است که از آن برای تسهیل ارتباط و هماهنگی بین اجزای یک سیستم توزیع شده استفاده می‌شود. در این الگو یک موجودیت مرکزی به نام broker وظیفه ارتباط بین اجزا را برعهده دارد که این امر به decoupling کمک می‌کند.",
-        "createdAt": 1703613489000
+        "createdAt": 1703613489000,
+        "status": "open"
     }]
 
     const columns = [
@@ -52,14 +77,14 @@ const AllTicketsPage = () => {
             },
             render: (value) => value
         },
-        // {
-        //     title: 'وضعیت',
-        //     dataIndex: 'status',
-        //     sorter: (a, b, sortOrder) => {
-        //         sorter('status', sortOrder);
-        //     },
-        //     render: (value) => <FeedbackStatus status={value} />,
-        // },
+        {
+            title: 'وضعیت',
+            dataIndex: 'status',
+            sorter: (a, b, sortOrder) => {
+                sorter('status', sortOrder);
+            },
+            render: (value) => value,
+        },
         {
             title: 'پیام',
             dataIndex: 'message',
@@ -76,14 +101,9 @@ const AllTicketsPage = () => {
     ];
 
     function sorter(sortBy, sortOrder) {
-        sortOrder = sortOrder === 'descend' ? 'desc' : 'asc';
+        sortOrder = sortOrder === 'descend' ? 'DESC' : 'ASC';
 
-        // dispatch(
-        //   setFeedbacksSort({
-        //     sortBy,
-        //     sortOrder,
-        //   }),
-        // );
+
     }
 
     function sliceText(value, n = 75) {
@@ -102,7 +122,10 @@ const AllTicketsPage = () => {
     return (
         <div>
             <Header style={{ padding: "0px 30px", background: colorBgContainer, borderRadius: 15 }}>
-                همه تیکت‌ها
+                <span>
+                    همه تیکت‌ها
+                </span>
+                <Filter setFilterValues={setFilter} />
             </Header>
             <Table
                 // onRow={(record) => {
