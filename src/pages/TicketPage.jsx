2cb3f6aada4e2f52a28d29215f101c60402374d7
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2024-01-02 16:22:45 +0330

    Add mock data and create show status in farsi by status in back-end

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -1,18 +1,28 @@
-import NavBar from "../components/navBar";
+import { useState } from "react";
 
+const ticketStatus = {
+    "waiting_for_admin" : "در انتظار اساین شدن",
+    "closed" : "بسته شده",
+    "in_progress" : "در جریان"
+}
 
 const TicketPage = () => {
 
+    const [ticketInfo, setTicketInfo] = useState({
+        "id" : "110011",
+        "title": "کند شدن تیغه مخلوط‌کن",
+        "description" : "تیغه مخلوط‌کن من کند شده الان باید چیکار کنم؟",
+        "created_by" : {"name" : "غلام"},
+        "asignee" : {"name" : "پاسخگوی ۱۱"},
+        "organization" : {"name" : "پارس خزر"},
+        "status" : "waiting_for_admin",
+        "type" : "bug",
+        "deadline" : "2024-01-22T22:20:57.390+00:00",
+        "created_at" : "2024-01-01T22:20:57.390+00:00",
+        "updated_at" : "2024-01-02T22:20:57.390+00:00"
+    });
     return (
         <div>
-            <div>
-                <div className="site-header-bg"></div>
-                <NavBar />
-            </div>
-            <div className="ticket-form">
-                <div>
-                </div>
-            </div>
         </div>
     )
 }
