1c036cf82548bb8e288cd6f5f1c26be6e041d890
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2024-01-30 05:34:44 +0330

    Remove required deadline feature

--- src/pages/TicketRegister.jsx
+++ src/pages/TicketRegister.jsx
@@ -112,7 +112,7 @@ const TicketRegister = () => {
                             </Radio.Group>
                         </Form.Item >
                         <FormItem name="deadline" label="ددلاین‌ برای پاسخگویی" rules={[
-                            { required: true, message: "لطفا توضیحات تیکت را وارد کنید." },
+                            { message: "لطفا توضیحات تیکت را وارد کنید." },
                         ]}>
                             <DatePicker onChange={onChange} />
                         </FormItem>
